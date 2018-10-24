9617cf00b87699bda95c786c86fc2592a08e467a
Ryan Kendrick <rpkendrick@gmail.com>
2018-10-24 16:46:56 +0000

    completed character counting exercise

--- countLetters.js
+++ countLetters.js
@@ -1,7 +1,21 @@
-var args = process.argv[2];
+var args = process.argv.slice(2);
 
 function countLetters(string) {
+  string = string.join('').toLowerCase();
   var charCount = {};
+  charCount[string[0]] = 1;
+  var key = string[0];
+  for (var i = 1; i < string.length; i++) {
+    var letter = string[i];
+    if (key.includes(letter)) {
+     charCount[letter]++;
+    } else {
+      charCount[letter] = 1;
+      key += letter;
+    }
+  }
+  return charCount;
 
+};
 
-}
+console.log(countLetters(args));
